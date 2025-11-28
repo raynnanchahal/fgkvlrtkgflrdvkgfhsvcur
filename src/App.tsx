280c7ecd0bf25964f8b01236beed607e977e4157
raynnanchahal <raynnanchahal@gmail.com>
2025-11-28 13:29:18 +0530

    Update App.tsx
    adding routing for policy pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,34 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
-import Onboarding from "./pages/Onboarding";
-import PrivacyPolicy from "./pages/PrivacyPolicy";
-import Terms from "./pages/Terms";
-import GDPR from "./pages/GDPR";
-import Disclaimer from "./pages/Disclaimer";
-import Refunds from "./pages/Refunds";
-import NotFound from "./pages/NotFound";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "@/pages/Home";
 
-const queryClient = new QueryClient();
+// Policy pages
+import PrivacyPolicy from "@/pages/PrivacyPolicy";
+import Terms from "@/pages/Terms";
+import GDPR from "@/pages/GDPR";
+import Refunds from "@/pages/Refunds";
+import Disclaimer from "@/pages/Disclaimer";
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/onboarding" element={<Onboarding />} />
-          <Route path="/privacy" element={<PrivacyPolicy />} />
-          <Route path="/terms" element={<Terms />} />
-          <Route path="/gdpr" element={<GDPR />} />
-          <Route path="/disclaimer" element={<Disclaimer />} />
-          <Route path="/refunds" element={<Refunds />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
+function App() {
+  return (
+    <Router>
+      <Routes>
+
+        {/* Main site */}
+        <Route path="/" element={<Home />} />
+
+        {/* Policy pages */}
+        <Route path="/privacy" element={<PrivacyPolicy />} />
+        <Route path="/terms" element={<Terms />} />
+        <Route path="/gdpr" element={<GDPR />} />
+        <Route path="/refunds" element={<Refunds />} />
+        <Route path="/disclaimer" element={<Disclaimer />} />
+
+        {/* Fallback – redirect unknown pages back to Home */}
+        <Route path="*" element={<Home />} />
+
+      </Routes>
+    </Router>
+  );
+}
 
 export default App;
