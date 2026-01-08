fcadad9604d713ed212f1f6e19616a51e0a4ccb0
raynnanchahal <raynnanchahal@gmail.com>
2026-01-08 21:12:40 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -11,36 +11,30 @@ import Disclaimer from "@/pages/Disclaimer";
 // Onboarding page
 import Onboarding from "@/pages/Onboarding";
 
-// Hidden pages
-import Moments from "@/pages/Moments";
-
 function App() {
-return (
-<Router>
-<Routes>
+  return (
+    <Router>
+      <Routes>
 
-{/* Main site */}
-<Route path="/" element={<Home />} />
+        {/* Main site */}
+        <Route path="/" element={<Home />} />
 
-{/* Onboarding */}
-<Route path="/onboarding" element={<Onboarding />} />
+        {/* Onboarding */}
+        <Route path="/onboarding" element={<Onboarding />} />
 
-{/* Hidden Pages */}
-<Route path="/moments" element={<Moments />} />
+        {/* Policy pages */}
+        <Route path="/privacy" element={<PrivacyPolicy />} />
+        <Route path="/terms" element={<Terms />} />
+        <Route path="/gdpr" element={<GDPR />} />
+        <Route path="/refunds" element={<Refunds />} />
+        <Route path="/disclaimer" element={<Disclaimer />} />
 
-{/* Policy pages */}
-<Route path="/privacy" element={<PrivacyPolicy />} />
-<Route path="/terms" element={<Terms />} />
-<Route path="/gdpr" element={<GDPR />} />
-<Route path="/refunds" element={<Refunds />} />
-<Route path="/disclaimer" element={<Disclaimer />} />
+        {/* Fallback – redirect unknown pages back to Home */}
+        <Route path="*" element={<Home />} />
 
-{/* Fallback – redirect unknown pages back to Home */}
-<Route path="*" element={<Home />} />
-
-</Routes>
-</Router>
-);
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
